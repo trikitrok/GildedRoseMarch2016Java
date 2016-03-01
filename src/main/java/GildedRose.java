ed7170cdfde16e6f478c77ff526d80be0754fc46
trikitrok <trikitrok@gmail.com>
2016-03-01 12:15:18 +0100

    Extracted method to decrement quality

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -18,11 +18,7 @@ public class GildedRose {
         }
 
         if (!isAgedBrie(item) && !isBackstagePasses(item)) {
-            if (item.quality > 0) {
-
-                item.quality = item.quality - 1;
-
-            }
+            decrementQuality(item);
         } else {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
@@ -48,9 +44,7 @@ public class GildedRose {
         if (item.sellIn < 0) {
             if (!isAgedBrie(item)) {
                 if (!isBackstagePasses(item)) {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
+                    decrementQuality(item);
                 } else {
                     item.quality = item.quality - item.quality;
                 }
@@ -59,6 +53,12 @@ public class GildedRose {
                     item.quality = item.quality + 1;
                 }
             }
+        }
+    }
+
+    private void decrementQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
         }
     }
 
