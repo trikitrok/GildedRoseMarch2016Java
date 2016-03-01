0c6adeeb881b0f15e7426613727a56c7caa6fd64
trikitrok <trikitrok@gmail.com>
2016-03-01 13:10:43 +0100

    Inlining method

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -8,18 +8,14 @@ public class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             Item item = items[i];
-            updateItemQuality(item);
+            if (isSulfuras(item)) {
+                return;
+            }
+
+            MutableItem mutableItem = new MutableItem(item);
+
+            mutableItem.degrade();
         }
-    }
-
-    private void updateItemQuality(Item item) {
-        if (isSulfuras(item)) {
-            return;
-        }
-
-        MutableItem mutableItem = new MutableItem(item);
-
-        mutableItem.degrade();
     }
 
     private boolean isSulfuras(Item item) {
