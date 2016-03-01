4a383af864578f5e0b819f5fdcae5a894ccbda11
trikitrok <trikitrok@gmail.com>
2016-03-01 12:59:19 +0100

    Introduced MutableItem wrapper

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -17,75 +17,12 @@ public class GildedRose {
             return;
         }
 
-        if (isAgedBrie(item)) {
-            incrementQuality(item);
+        MutableItem mutableItem = new MutableItem(item);
 
-            age(item);
-
-            if (item.sellIn < 0) {
-                incrementQuality(item);
-            }
-        }
-
-        if (isBackstagePasses(item)) {
-            incrementQuality(item);
-
-            if (item.sellIn < 11) {
-                incrementQuality(item);
-            }
-
-            if (item.sellIn < 6) {
-                incrementQuality(item);
-            }
-
-            age(item);
-
-            if (item.sellIn < 0) {
-                item.quality = 0;
-            }
-        }
-
-        if (isPerishable(item)) {
-            decrementQuality(item);
-
-            age(item);
-
-            if (item.sellIn < 0) {
-                decrementQuality(item);
-            }
-        }
-
-    }
-
-    private void age(Item item) {
-        item.sellIn = item.sellIn - 1;
-    }
-
-    private boolean isPerishable(Item item) {
-        return !isAgedBrie(item) && !isBackstagePasses(item);
-    }
-
-    private void incrementQuality(Item item) {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1;
-        }
-    }
-
-    private void decrementQuality(Item item) {
-        if (item.quality > 0) {
-            item.quality = item.quality - 1;
-        }
+        mutableItem.degrade();
     }
 
     private boolean isSulfuras(Item item) {
         return item.name.equals("Sulfuras, Hand of Ragnaros");
-    }
-
-    private boolean isBackstagePasses(Item item) {
-        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
-    }
-
-    private boolean isAgedBrie(Item item) {
-        return item.name.equals("Aged Brie");
     }
 }
