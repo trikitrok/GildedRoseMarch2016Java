7a044c41fdaa8f58595effca042cb69fc64f4f5b
trikitrok <trikitrok@gmail.com>
2016-03-01 12:12:42 +0100

    Used a guard clause for Sulfuras special case to eliminate some nested conditions

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -13,11 +13,15 @@ public class GildedRose {
     }
 
     private void updateItemQuality(Item item) {
+        if (isSulfuras(item)) {
+            return;
+        }
+
         if (!isAgedBrie(item) && !isBackstagePasses(item)) {
             if (item.quality > 0) {
-                if (!isSulfuras(item)) {
-                    item.quality = item.quality - 1;
-                }
+
+                item.quality = item.quality - 1;
+
             }
         } else {
             if (item.quality < 50) {
@@ -39,17 +43,13 @@ public class GildedRose {
             }
         }
 
-        if (!isSulfuras(item)) {
-            item.sellIn = item.sellIn - 1;
-        }
+        item.sellIn = item.sellIn - 1;
 
         if (item.sellIn < 0) {
             if (!isAgedBrie(item)) {
                 if (!isBackstagePasses(item)) {
                     if (item.quality > 0) {
-                        if (!isSulfuras(item)) {
-                            item.quality = item.quality - 1;
-                        }
+                        item.quality = item.quality - 1;
                     }
                 } else {
                     item.quality = item.quality - item.quality;
