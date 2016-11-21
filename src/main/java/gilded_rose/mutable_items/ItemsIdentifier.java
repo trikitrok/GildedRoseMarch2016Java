18ddf30e0d94fb35bc0ecbc74e3e02534914d300
trikitrok <trikitrok@gmail.com>
2016-11-21 15:24:58 +0100

    making methods package local

--- src/main/java/gilded_rose/mutable_items/ItemsIdentifier.java
+++ src/main/java/gilded_rose/mutable_items/ItemsIdentifier.java
@@ -1,32 +1,32 @@
 package gilded_rose.mutable_items;
 
 class ItemsIdentifier {
-    public static final String CONJURED_MARK = "Conjured";
-    public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
-    public static final String AGED_BRIE = "Aged Brie";
-    public static final String BACKSTAGE_PASSES_TO_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String CONJURED_MARK = "Conjured";
+    private static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
+    private static final String AGED_BRIE = "Aged Brie";
+    private static final String BACKSTAGE_PASSES_TO_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
 
-    public static boolean isPerishable(String name) {
+    static boolean isPerishable(String name) {
         return !isAgedBrie(name) && !isBackstagePasses(name);
     }
 
-    public static boolean isBackstagePasses(String name) {
+    static boolean isBackstagePasses(String name) {
         return name.equals(BACKSTAGE_PASSES_TO_CONCERT);
     }
 
-    public static boolean isAgedBrie(String name) {
+    static boolean isAgedBrie(String name) {
         return name.equals(AGED_BRIE);
     }
 
-    public static boolean isSulfuras(String name) {
+    static boolean isSulfuras(String name) {
         return name.contains(SULFURAS_HAND_OF_RAGNAROS);
     }
 
-    public static boolean isConjured(String name) {
+    static boolean isConjured(String name) {
         return name.contains(CONJURED_MARK);
     }
 
-    public static String extractNotConjuredName(String name) {
+    static String extractNotConjuredName(String name) {
         return name.replace(CONJURED_MARK, "").trim();
     }
 }
